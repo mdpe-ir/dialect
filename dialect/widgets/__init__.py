de22dcb2cfe0a7c0e4797b7a0ac1aff667ed504f
Mahan <57597379+mdpe-ir@users.noreply.github.com>
2023-06-26 13:21:54 +0330

    Delete dialect directory

--- dialect/widgets/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright 2020-2022 Mufeed Ali
-# Copyright 2020-2022 Rafael Mardojai CM
-# SPDX-License-Identifier: GPL-3.0-or-later
-
-from dialect.widgets.lang_selector import LangSelector # noqa
-from dialect.widgets.provider_preferences import ProviderPreferences  # noqa
-from dialect.widgets.theme_switcher import ThemeSwitcher  # noqa
